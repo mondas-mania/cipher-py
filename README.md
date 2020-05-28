be8ebfe836e37b49d1b944b3727e65c208e00086
David Sampson <dcsampsonlaptop@gmail.com>
2020-05-28 19:07:08 +0100

    Atbash cipher & aP mention

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cryptography Code
-Tinkering around with code for solving common ciphers.
+Tinkering around with code for solving common ciphers.  
+
+Inspired by my love for the Sci-fi podcast [ars Paradoxica](https://www.whisperforge.org/arsparadoxica), into which I have poured many hours attempting to crack the number stations after each episode, loving every second.
--- script.py
+++ script.py
@@ -13,7 +13,7 @@ clean_input = []
 
 def caesar_encode(plain_txt, shift, alphabet_str=string.ascii_lowercase):
     alph_len = len(alphabet_str)
-    new_alphabet = create_alphabet(alphabet_str[shift % alph_len:], alphabet_str)
+    new_alphabet = keyed_alphabet(alphabet_str[shift % alph_len:], alphabet_str)
     return substitution(plain_txt, alphabet_str, new_alphabet)
 
 def caesar_decode(encoded_text, shift, alphabet_str=string.ascii_lowercase):
@@ -22,18 +22,18 @@ def caesar_decode(encoded_text, shift, alphabet_str=string.ascii_lowercase):
 ### Keyed Caesar
 
 def keyed_caesar_encode(plain_txt, shift, alphabet_key, alphabet_str=string.ascii_lowercase):
-    new_alphabet = create_alphabet(alphabet_key, alphabet_str)
+    new_alphabet = keyed_alphabet(alphabet_key, alphabet_str)
     new_txt = caesar_encode(plain_txt, shift, alphabet_str)
     return substitution(new_txt, alphabet_str, new_alphabet)
 
 def keyed_caesar_decode(encoded_text, shift, alphabet_key, alphabet_str=string.ascii_lowercase):
-    new_alphabet = create_alphabet(alphabet_key, alphabet_str)
+    new_alphabet = keyed_alphabet(alphabet_key, alphabet_str)
     new_txt = substitution(encoded_text, new_alphabet, alphabet_str)
     return caesar_decode(new_txt, shift, alphabet_str)
 
 # Create a keyed alphabet
 
-def create_alphabet(alphabet_key, alphabet_str=string.ascii_lowercase):
+def keyed_alphabet(alphabet_key, alphabet_str=string.ascii_lowercase):
     new_alphabet = [char for char in alphabet_str]
     new_alphabet_key = ''.join(sorted(set(alphabet_key), key=alphabet_key.index))
     [new_alphabet.remove(char) for char in new_alphabet_key]
@@ -47,7 +47,17 @@ def substitution(input, alphabet, new_alphabet):
     new_txt = ''.join([new_alphabet[id] for id in idx])
     return new_txt
 
+# Atbash Cipher
+
+def invert_alphabet(alphabet_str=string.ascii_lowercase):
+    return alphabet_str[::-1]
+
+def atbash(plain_txt, alphabet_str=string.ascii_lowercase):
+    new_alphabet = invert_alphabet(alphabet_str)
+    return substitution(plain_txt, alphabet_str, new_alphabet)
+
 encoded = caesar_encode(clean_input[0], 1)
 print(caesar_decode(encoded, 1))
 encoded2 = (keyed_caesar_encode("helloworld", 1, "hello"))
 print(keyed_caesar_decode(encoded2, 1, "hello"))
+print(atbash("abcd"))
